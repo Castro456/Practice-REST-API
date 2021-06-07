0707d4879e19adacea9745ec840162b3d80476a6
Castro456 <castrosid456@gmail.com>
2021-06-07 16:24:24 +0530

    json decode explain

--- view/create.php
+++ view/create.php
@@ -15,8 +15,7 @@ $student=new Student($connection); //$connection is the parameter passed to cons
 // $student object is assigned to access the properties of class Student like declared variables public name,email,mobile
 if ($_SERVER['REQUEST_METHOD']==="POST") {
   $data=json_decode(file_get_contents("php://input"));
-
-  // way to get all the data from body section of the req parameter
+  // way to get all the data of the body parameter
   if (( !empty($data->name)) && ( !empty($data->name)) && ( !empty($data->name))) {
     // print_r($data);
     $student->name=$data->name;
